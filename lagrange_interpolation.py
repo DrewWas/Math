d494c8790580fe7f5984beaa8b573db2b479df35
Andrew Wasielewski <awwasielewski@gmail.com>
2023-06-27 23:27:19 +0200

    fixed plot. dont feel like sortin the linked list

--- lagrange_interpolation.py
+++ lagrange_interpolation.py
@@ -1,23 +1,22 @@
 #https://en.wikipedia.org/wiki/Lagrange_polynomial
 
 import matplotlib.pyplot as plt
-from random import randint
+from random import sample
 
 
 def plot_points(num_of_points, ceiling):
-    x_points = [i for i in range(0, num_of_points)]
-    y_points = [randint(0, ceiling) for i in range(0, num_of_points)]
+    x_points = sample(range(0,ceiling), num_of_points)
+    y_points = sample(range(0,ceiling), num_of_points)
     points = [(x_points[i], y_points[i]) for i in range(0, num_of_points)]
     print(points)
 
     plt.plot(x_points, y_points, "o")
-    plt.xlim(-1, num_of_points + 1)
+    plt.xlim(-1, ceiling + 1)
     plt.ylim(-ceiling, ceiling + 1)
     plt.show()
     return points 
 
 
 
-x = plot_points(10, 10)
-x
-    
+plot_points(3, 10)
+
---
+++ test.py
@@ -0,0 +1,8 @@
+from random import sample, randint
+
+nums = [sample(range(0,20), 15)]
+
+#x = sample(nums, 9)
+
+print(nums)
+#print(x)
